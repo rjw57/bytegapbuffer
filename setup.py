1e01bc8b4a94fd14b7f4696ac5b0f522f389b6a8
Rich Wareham <rjw57@cam.ac.uk>
2015-08-24 22:02:41 +0100

    release version with new metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytegapbuffer",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=['test']),
     install_requires=[
         'future', # py2/3 compat
