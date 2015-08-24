40512841ab02e862c37ed5a150a128306d6783a1
Rich Wareham <rjw57@cam.ac.uk>
2015-08-24 21:54:36 +0100

    add some metadata for submission to PyPI

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytegapbuffer",
-    version="0.1",
+    description="A bytearray work-alike using a gap buffer for storage",
+    version="0.1.0",
     packages=find_packages(exclude=['test']),
     install_requires=[
         'future', # py2/3 compat
