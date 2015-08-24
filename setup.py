acf6fd48481583c7a55925a2f4010858c6d7cf70
Rich Wareham <rjw57@cam.ac.uk>
2015-08-24 22:01:43 +0100

    setup: add some more metadata

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytegapbuffer",
-    description="A bytearray work-alike using a gap buffer for storage",
     version="0.1.0",
     packages=find_packages(exclude=['test']),
     install_requires=[
