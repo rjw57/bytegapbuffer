d093bd7db2c6630cd2307962de42bbeadccc4fb8
Rich Wareham <rjw57@cam.ac.uk>
2015-09-02 16:35:46 +0100

    prepare for 0.9.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bytegapbuffer",
-    version="0.9.1",
+    version="0.9.2",
     packages=find_packages(exclude=['test']),
     description="A bytearray work-alike using a gap buffer for storage",
     author="Rich Wareham",
